05643aed40b3d2605f06a6f6755f9f20614ac5bf
Chandra Lokesh Chary Girimapuram(UST <286199@ust.com>
2025-06-05 11:10:50 +0530

    Created my-java-library gradle project

---
+++ my-java-library/lib/src/main/java/mylibrary/Library.java
@@ -0,0 +1,11 @@
+package mylibrary;
+
+public class Library {
+    public static String greeting() {
+        return "Hello from My Java Library!";
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
